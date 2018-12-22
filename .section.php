80550cc3da8a595081e81dbc099321d6359b1acd
Gordey Babich <magnumcumlatte@yandex.ru>
2018-12-22 18:51:19 +0300

    git-rep create

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Купи пароварку от Гул Дана! Йогг-Сарон рекоменует!",
+   "keywords" => "пароварка, пароварки, варкрафт, warcraft",
+   "robots" => "index, follow"
+);
+?>
